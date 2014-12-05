4ba154676fe0b9761795b86863a673ab419cd0d7
Brett Israelsen <brett.israelsen@colorado.edu>
2014-12-05 12:45:43 -0700

    Brett - added global dist to Tree_calcs

--- Tree_calcs.py
+++ Tree_calcs.py
@@ -8,7 +8,7 @@ import sys
 import scipy
 
 #graph_loc = sys.argv[1]
-graph_loc = "/home/brett/brett.israelsen@gmail.com/Education/Graduate/Fall 2014/CS5352/Project/saved_graphs/Tree_c_3_l_10.g.xml.gz"
+graph_loc = "/home/brett/brett.israelsen@gmail.com/Education/Graduate/Fall 2014/CS5352/Project/saved_graphs/Tree_c_2_l_10.g.xml.gz"
 g = load_graph(graph_loc)
 N = g.num_vertices()
 
@@ -22,12 +22,29 @@ def do_stuff(grp, m_grp,strProp):
     for i in range(0,len(g_dist)):
         sys.stdout.write(str(g_dist[i]) + " ")
     print("")
+    return g_dist
     
 for grp in list(set(b_grp)):
     sys.stdout.write("bayes ")
-    do_stuff(grp, b_grp,'bayes')
+    g_dist = do_stuff(grp, b_grp,'bayes')
+    
 
+    global_dist = []
+    for i in list(set(b_grp)):
+        gd = float(len(g_dist[g_dist ==i]))/ float(len(b_grp[b_grp == i]))
+        global_dist.append(gd)
+        sys.stdout.write(str(gd) + " ")
+    print("")
 
 for grp in list(set(m_grp)):
     sys.stdout.write("mdl ")
-    do_stuff(grp, m_grp,'mdl')
+    g_dist = do_stuff(grp, b_grp,'mdl')
+    
+
+    global_dist = []
+    for i in list(set(b_grp)):
+        gd = len(g_dist[g_dist ==i])/ len(b_grp[b_grp == i])
+        global_dist.append(gd)
+        sys.stdout.write(str(gd) + " ")
+    print("")
+
