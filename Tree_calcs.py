3717075c5d1507231d79da3f716c7a33e7a35822
Brett Israelsen <brett.israelsen@colorado.edu>
2014-12-04 22:50:35 -0700

    Brett - renamed Tree_calcs to Graph_calcs, made new Tree_calcs

--- Tree_calcs.py
+++ Tree_calcs.py
@@ -7,46 +7,22 @@ import numpy as np
 import sys
 import scipy
 
-graph_loc = sys.argv[1]
-#graph_loc = "/home/brett/brett.israelsen@gmail.com/Education/Graduate/Fall 2014/CS5352/Project/saved_graphs/karate.g.xml.gz"
+#graph_loc = sys.argv[1]
+graph_loc = "/home/brett/brett.israelsen@gmail.com/Education/Graduate/Fall 2014/CS5352/Project/saved_graphs/Tree_c_3_l_10.g.xml.gz"
 g = load_graph(graph_loc)
 N = g.num_vertices()
 
 b_grp = g.vp['bayes'].a
 m_grp = g.vp['mdl'].a
-
-def diffEdgeType(g,str_v_prop,grp_src):
-    isDifferent = g.new_edge_property('bool')
-    
-    for e in g.edges():
-        if g.vp[str_v_prop].a[e.source()] == grp_src or g.vp[str_v_prop].a[e.target()] == grp_src:
-            if not g.vp[str_v_prop].a[e.source()] == g.vp[str_v_prop].a[e.target()]:
-                isDifferent[e] = True
-            else:
-                isDifferent[e] = False
-        else:
-            isDifferent[e] = False
-            
-    return isDifferent
+l_grp = g.vp['Level'].a
             
 def do_stuff(grp, m_grp,strProp):
-    nvN = sum(m_grp == grp)/float(N)
-    sys.stdout.write(str(nvN) + " ")
-    
-    g2 = GraphView(g, vfilt=lambda e: m_grp[e] == grp)
-    avg = vertex_average(g2, 'total')
-    sys.stdout.write(str(avg[0]) + " ")
-    sys.stdout.write(str(avg[1]) + " ")
-    
-    ed = g2.num_edges() / scipy.misc.comb(g2.num_vertices(), 2)
-    sys.stdout.write(str(ed))
+    g_dist = l_grp[m_grp == grp]
 
-    E = g2.num_edges()
-    g3 = GraphView(g,efilt = diffEdgeType(g, strProp,grp))
-    E_dens = float(E)/float(g3.num_edges())
-    sys.stdout.write(str(E_dens))
+    for i in range(0,len(g_dist)):
+        sys.stdout.write(str(g_dist[i]) + " ")
     print("")
-
+    
 for grp in list(set(b_grp)):
     sys.stdout.write("bayes ")
     do_stuff(grp, b_grp,'bayes')
